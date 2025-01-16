15ea7d993b5eb6a09bf105b4dc927db913e1ec19
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-16 23:25:30 +0900

    feature: implement Chebyshev.Polynominal.of_2nd_kind/1

--- lib/chebyshev/polynominal.ex
+++ lib/chebyshev/polynominal.ex
@@ -1,2 +1,11 @@
 defmodule Chebyshev.Polynominal do
+  @spec of_2nd_kind(non_neg_integer()) :: (number() -> float())
+  def of_2nd_kind(0), do: fn _ -> 1.0 end
+  def of_2nd_kind(1), do: fn x -> 2.0 * x * of_2nd_kind(0).(x) end
+
+  def of_2nd_kind(n) when is_integer(n) and n >= 2 do
+    fn x ->
+      2.0 * x * of_2nd_kind(n - 1).(x) - of_2nd_kind(n - 2).(x)
+    end
+  end
 end
