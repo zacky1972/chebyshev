a8140c1c49c89ebf57ffbb50b5ccde801d05fc3b
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-16 23:07:59 +0900

    initial commit

---
+++ lib/chebyshev.ex
@@ -0,0 +1,18 @@
+defmodule Chebyshev do
+  @moduledoc """
+  Documentation for `Chebyshev`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chebyshev.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/chebyshev_test.exs
@@ -0,0 +1,8 @@
+defmodule ChebyshevTest do
+  use ExUnit.Case
+  doctest Chebyshev
+
+  test "greets the world" do
+    assert Chebyshev.hello() == :world
+  end
+end
